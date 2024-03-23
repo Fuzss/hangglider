6f4c31bd8726cd258c53b19de1356ecb061e04dc
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-03-23 22:45:25 +0100

    separate client data gen

--- 1.20.4/NeoForge/src/main/java/fuzs/hangglider/neoforge/HangGliderNeoForge.java
+++ 1.20.4/NeoForge/src/main/java/fuzs/hangglider/neoforge/HangGliderNeoForge.java
@@ -3,9 +3,6 @@ package fuzs.hangglider.neoforge;
 import fuzs.hangglider.HangGlider;
 import fuzs.hangglider.data.ModItemTagProvider;
 import fuzs.hangglider.data.ModRecipeProvider;
-import fuzs.hangglider.data.client.ModLanguageProvider;
-import fuzs.hangglider.data.client.ModModelProvider;
-import fuzs.hangglider.neoforge.data.client.ModSpriteSourceProvider;
 import fuzs.puzzleslib.api.core.v1.ModConstructor;
 import fuzs.puzzleslib.neoforge.api.data.v2.core.DataProviderHelper;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -21,10 +18,7 @@ public class HangGliderNeoForge {
         ModConstructor.construct(HangGlider.MOD_ID, HangGlider::new);
         DataProviderHelper.registerDataProviders(HangGlider.MOD_ID,
                 ModItemTagProvider::new,
-                ModModelProvider::new,
-                ModLanguageProvider::new,
-                ModRecipeProvider::new,
-                ModSpriteSourceProvider::new
+                ModRecipeProvider::new
         );
     }
 }
--- 1.20.4/NeoForge/src/main/java/fuzs/hangglider/neoforge/client/HangGliderNeoForgeClient.java
+++ 1.20.4/NeoForge/src/main/java/fuzs/hangglider/neoforge/client/HangGliderNeoForgeClient.java
@@ -2,7 +2,11 @@ package fuzs.hangglider.neoforge.client;
 
 import fuzs.hangglider.HangGlider;
 import fuzs.hangglider.client.HangGliderClient;
+import fuzs.hangglider.data.client.ModLanguageProvider;
+import fuzs.hangglider.data.client.ModModelProvider;
+import fuzs.hangglider.neoforge.data.client.ModSpriteSourceProvider;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
+import fuzs.puzzleslib.neoforge.api.data.v2.core.DataProviderHelper;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -14,5 +18,10 @@ public class HangGliderNeoForgeClient {
     @SubscribeEvent
     public static void onConstructMod(final FMLConstructModEvent evt) {
         ClientModConstructor.construct(HangGlider.MOD_ID, HangGliderClient::new);
+        DataProviderHelper.registerDataProviders(HangGlider.MOD_ID,
+                ModLanguageProvider::new,
+                ModModelProvider::new,
+                ModSpriteSourceProvider::new
+        );
     }
 }
